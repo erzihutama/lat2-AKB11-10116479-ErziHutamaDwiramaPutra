de89f3686ed26fda2f973a4b1714334fe4bb13cb
Erzi Hutama Dwirama Putra <erzihutama@gmail.com>
2019-04-16 15:37:55 +0700

    fix button regist

--- app/src/main/java/erzi/akb11/myapplication/LoginActivity.java
+++ app/src/main/java/erzi/akb11/myapplication/LoginActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
 /*
 * tanggal : 14 april 2019 jam 10 pagi
 * 10116479
@@ -21,13 +23,16 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        login = (Button)findViewById(R.id.BtnLogin);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent loginn  = new Intent(getApplicationContext(),RegisterActivity.class);
-                startActivity(loginn);
-            }
-        });
+
+    }
+
+    public void regis(View view) {
+        Intent intent = null;
+        switch(view.getId()){
+            case R.id.btn_regist:
+                intent = new Intent(this,RegisterActivity.class);
+                break;
+        }
+        if (null!=intent) startActivity(intent);
     }
 }
